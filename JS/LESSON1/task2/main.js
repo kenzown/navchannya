93033c31a291fcc3eaa525ae2c9aacfe78d0b074
kenzown <marchykigor1@gmail.com>
2022-04-14 22:57:27 +0300

    js hm task 1-2

---
+++ JS/LESSON1/task2/main.js
@@ -0,0 +1,72 @@
+let v = 'hello';
+let b = "owu"
+let c = "com";
+let g = "ua"
+let t = "1"
+let y = "10"
+let u = "-999"
+let i = "123"
+let o = "3.14"
+let p = "2.7"
+let a = "16"
+let s = "true"
+let d = "false"
+console.log(v, b, c, g, t, y, u, i, o, p, a, s, d)
+alert(v);
+alert(b);
+alert(c);
+alert(g);
+alert(t);
+alert(y);
+alert(u);
+alert(i);
+alert(o);
+alert(p);
+alert(a);
+alert(s);
+alert(d);
+document.write(v, b, c, g, t, y, u, i, o, p, a, s, d);
+let book1 = {
+    name: "Terror",
+    count: 768,
+    genre: "history_novel"
+
+}
+let book2 = {
+    name: "Танець недоумка",
+    count: 642,
+    genre: "sci-fi, thiller",
+    authors: [
+        "Іларіон Павлюк",
+        "idqmdmwq",
+        "eqeqwewqeqwewq"
+    ]
+
+}
+
+console.log(book1, book2)
+const firstName = " Ihor",
+    middleName = "Marchuk",
+    lastName = "Vitaliyovuch";
+
+const person = firstName + " " + middleName + " " + lastName
+console.log(person)
+
+let Ihor = prompt("Ihor")
+let Marchuk = prompt("Marchuk")
+let Vitaliyovuch = prompt("Vitaliyovuch")
+console.log(Ihor, Marchuk, Vitaliyovuch)
+let a1 = 100;
+let b2 ='100';
+let c3 =true;
+console.log(a1, b2, c3)
+console.log(typeof 100)
+console.log(typeof '100')
+console.log(typeof true)
+let books = [
+    {name:'Вбивство в східному експресі', count:350, genre:'detective', author:'Agata Kristi'},
+    {name:'1984', count: 540, genre: 'dystopia', author:'Джордж Орвелл'}
+
+]
+console.log(books[0])
+console.log(books[1])
