ddfe9a4837d31ea3a2559837e2a1073ec0f02b0d
kenzown <marchykigor1@gmail.com>
2022-04-18 14:56:51 +0300

    js hm task 1-2

--- JS/LESSON1/task1/main2.js
+++ JS/LESSON1/task1/main2.js
@@ -1,22 +1,22 @@
-let a =5 < 6
+let a = 5 < 6
 console.log(a)
 let b = 5 > 6
 console.log(b)
-let c = 5/2 > 6
+let c = 5 / 2 > 6
 console.log(c)
 let n = 5 === 6
 console.log(n)
 let m = 10 === 10
 console.log(m)
-let cx = 10 == "10"
+let cx = 10 <= 10
 console.log(cx)
 let nj = 10 !== 10
 console.log(nj)
-let mk = 10 === "10"
+let mk = 10 >= '10'
 console.log(mk)
-let a2 = 10 != '10'
+let a2 = 10 <= '10'
 console.log(a2)
-let a3= 123 < 123
+let a3 = 123 < 123
 console.log(a3)
-let a4= 123 >= 123
+let a4 = 123 >= 123
 console.log(a4)
--- JS/LESSON1/task2/main.js
+++ JS/LESSON1/task2/main.js
@@ -1,17 +1,42 @@
 let v = 'hello';
+console.log(v)
+document.write(v)
 let b = "owu"
+console.log(b)
+document.write(b)
 let c = "com";
+console.log(c)
+document.write(c)
 let g = "ua"
+console.log(g)
+document.write(g)
 let t = "1"
+console.log(t)
+document.write(t)
 let y = "10"
+console.log(y)
+document.write(y)
 let u = "-999"
+console.log(u)
+document.write(u)
 let i = "123"
+console.log(i)
+document.write(i)
 let o = "3.14"
+console.log(o)
+document.write(o)
 let p = "2.7"
+console.log(p)
+document.write(p)
 let a = "16"
+console.log(a)
+document.write(a)
 let s = "true"
+console.log(s)
+document.write(s)
 let d = "false"
-console.log(v, b, c, g, t, y, u, i, o, p, a, s, d)
+console.log(d)
+document.write(d)
 alert(v);
 alert(b);
 alert(c);
@@ -25,7 +50,6 @@ alert(p);
 alert(a);
 alert(s);
 alert(d);
-document.write(v, b, c, g, t, y, u, i, o, p, a, s, d);
 let book1 = {
     name: "Terror",
     count: 768,
@@ -57,15 +81,15 @@ let Marchuk = prompt("Marchuk")
 let Vitaliyovuch = prompt("Vitaliyovuch")
 console.log(Ihor, Marchuk, Vitaliyovuch)
 let a1 = 100;
-let b2 ='100';
-let c3 =true;
+let b2 = '100';
+let c3 = true;
 console.log(a1, b2, c3)
 console.log(typeof 100)
 console.log(typeof '100')
 console.log(typeof true)
 let books = [
-    {name:'Вбивство в східному експресі', count:350, genre:'detective', author:'Agata Kristi'},
-    {name:'1984', count: 540, genre: 'dystopia', author:'Джордж Орвелл'}
+    {name: 'Вбивство в східному експресі', count: 350, genre: 'detective', author: 'Agata Kristi'},
+    {name: '1984', count: 540, genre: 'dystopia', author: 'Джордж Орвелл'}
 
 ]
 console.log(books[0])
